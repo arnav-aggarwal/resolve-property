857fe6d23b58e0784b25b6c7951f720d9e57a168
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-06-05 11:26:18 -0700

    Add JSON test

--- tester.js
+++ tester.js
@@ -2,7 +2,6 @@ const tests = require('./tests.js');
 require('./index.js');
 
 tests.forEach(({ obj, path }) => {
-    console.log(obj, path)
     const prop = Object.extractProperty(obj, path);
     console.log(prop);
 });
