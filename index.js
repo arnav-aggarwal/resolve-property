2e2418ab771d0206617c9c36ddf107caf92a4c6f
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-06-06 21:52:54 -0700

    Add path testing

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+function testPath(path, matches) {
+  let dotIndex = path.indexOf('.');
+  let bracketIndex = path.indexOf('[');
+
+  let sliceIndex;
+  if (dotIndex === -1) {
+    sliceIndex = bracketIndex;
+  } else if (dotIndex === -1) {
+    sliceIndex = bracketIndex;
+  } else {
+    sliceIndex = Math.min(bracketIndex, dotIndex);
+  }
+
+  let shorterPath = path.slice(sliceIndex);
+  matches.forEach(match => {
+    const matchIndex = shorterPath.indexOf(match);
+    shorterPath = shorterPath.substring(0, matchIndex) + shorterPath.substring(matchIndex + match.length);
+  });
+
+  if(shorterPath.match(/[^.'"[\]]/)) {
+    throw new Error(`Invalid object path entered: ${shorterPath}`);
+  }
+}
+
 function extractPropertyNames(path) {
   // TODO: Allow escaped quote marks in property names
   // Simpler regex that allows more false positives: /([\w$]+(?=\[|\.|$))|("|')(?:.+?)[^\\]\2|\[\d+\]|\[('|").\3\]/
   // Allows something like obj.8.32
-  const parseMatch = str => /'|"|\[/.match(str[0]) ? str.slice(1, -1) : str;
-  return path.match(/([a-zA-Z_$](?:[\w$]+)?(?=\[|\.|$))|("|')(?:.+?)[^\\]\2|\[\d+\]|\[('|").\3\]/g)
+  const parseMatch = str => str[0].match(/'|"|\[/) ? str.slice(1, -1) : str;
+  const matches = path.match(/([a-zA-Z_$](?:[\w$]+)?(?=\[|\.|$))|("|')(?:.+?)[^\\]\2|\[\d+\]|\[('|").\3\]/g)
     .slice(1)
     .map(property => parseMatch(parseMatch(property)));
+  
+  console.log(matches)
+
+  testPath(path, matches);
+  
+  return matches;
 }
 
 function extractProperty(obj, path) {
