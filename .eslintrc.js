52081a212c3fa02ccf414c80bd951bed42ac9112
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-06-09 15:49:23 -0700

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'env': {
+        'es6': true,
+        'node': true,
+        'jest': true,
+    },
+    'extends': 'eslint:recommended',
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
